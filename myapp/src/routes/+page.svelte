cbfd1271d3670f1024590796fef80afd1314ab1a
dwseoh <dongwanseoh@gmail.com>
2024-08-24 11:23:51 -0400

    creation

---
+++ myapp/src/routes/+page.svelte
@@ -0,0 +1,16 @@
+
+<script>
+	let name = 'world';
+</script>
+
+<!-- <h1>Hello {name}!</h1> -->
+<h1>Baseball Speedgun Online</h1>
+
+<style>
+	h1 {
+		color: rgb(27, 157, 209);
+		font-family: 'DM Sans';
+		font-size: 4em;
+        text-align: center;
+	}
+</style>
