c8e7467190a08af03cde298a1dfc28b43257e321
dwseoh <dongwanseoh@gmail.com>
2024-09-07 17:03:48 -0400

    final touchups

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <svelte:head>
-<!--   <link rel="icon" href="/favicon.ico" type="image/x-icon">
-  <link rel="icon" href="/favicon-32x32.png" sizes="32x32" type="image/png">
-  <link rel="icon" href="/favicon-16x16.png" sizes="16x16" type="image/png">
-  <link rel="apple-touch-icon" href="/apple-touch-icon.png">
-  <meta name="theme-color" content="#ffffff"> -->
+	<title>Speedgun Online</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width,initial-scale=1" />
+	<link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="style.css" />
+	<link rel="icon" type="image/png" href="speedgun.svg" sizes="32x32" />
+	<link rel="icon" type="image/png" href="speedgun.svg" sizes="16x16" />
 </svelte:head>
 
 
@@ -125,13 +127,13 @@
 	</footer>
 
 </div>
-<!--  later use variables for device compatability in html -->
 
 
 <style>
 
 
-
+	@import url('https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap');	
+	
 	.all{
 		font-family: 'DM Sans';
 		color: rgb(209, 208, 208);
